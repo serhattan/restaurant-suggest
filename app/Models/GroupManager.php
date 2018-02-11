06fd87c90477770a6f844fc82704de86651f55f9
Eren Akpınar <eren.akpinar@kolayik.com>
2018-02-11 13:41:14 +0300

    Add save function for group manager (WHAT-5)

--- app/Models/GroupManager.php
+++ app/Models/GroupManager.php
@@ -2,15 +2,38 @@
 
 namespace App\Models;
 
+use App\Helpers\Helper;
 use App\Models\DB\Group;
 use App\Models\Entity;
 use Illuminate\Support\Facades\Auth;
 
 class GroupManager
 {
+    /**
+     * @param $id
+     * @param array $with
+     * @return Entity\Group
+     */
+    public static function get($id, $with = [])
+    {
+        $group = Group::with($with)
+            ->where('id', $id)
+            ->where('status', Helper::STATUS_ACTIVE)
+            ->first();
+
+        if ($group instanceof Group) {
+            return self::map($group);
+        }
+    }
+
+    /**
+     * @return array
+     */
     public static function getAll()
     {
-        $groups = Group::where('user_id', Auth::id())->get();
+        $groups = Group::where('user_id', Auth::id())
+            ->where('status', Helper::STATUS_ACTIVE)
+            ->get();
 
         return self::multiMap($groups);
     }
@@ -43,5 +66,26 @@ class GroupManager
         }
 
         return $groupList;
+    }
+
+    /**
+     * @param $group
+     * @return bool
+     */
+    public static function save($group)
+    {
+        $newGroup = new Group();
+        if (isset($group['id']) && empty($group['id'])) {
+            $newGroup->id = Helper::generateId();
+            $newGroup->status = Helper::STATUS_ACTIVE;
+            $newGroup->created_by = Auth::id();
+        } else {
+            $newGroup = Group::where($group['id'])->first();
+        }
+
+        $newGroup->name = $group['name'];
+        $newGroup->budget = $group['budget'];
+
+        return $newGroup->save();
     }
 }
