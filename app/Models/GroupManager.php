5ebebfe9e71909d369ee014ee2e12e23abf875f4
Eren Akpınar <eren.akpinar@kolayik.com>
2018-02-11 12:58:58 +0300

    Add group manager functions (WHAT-5)

--- app/Models/GroupManager.php
+++ app/Models/GroupManager.php
@@ -1,10 +1,47 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\DB\Group;
+use App\Models\Entity;
+use Illuminate\Support\Facades\Auth;
 
 class GroupManager
 {
-    //
+    public static function getAll()
+    {
+        $groups = Group::where('user_id', Auth::id())->get();
+
+        return self::multiMap($groups);
+    }
+
+    /**
+     * @param Group $group
+     * @return Entity\Group
+     */
+    public static function map(Group $group)
+    {
+        $newGroup = new Entity\Group();
+        $newGroup->setId($group->id);
+        $newGroup->setName($group->name);
+        $newGroup->setBudget($group->budget);
+        $newGroup->setCreatedBy($group->created_by);
+        $newGroup->setStatus($group->status);
+
+        return $newGroup;
+    }
+
+    /**
+     * @param Group[] $groups
+     * @return array
+     */
+    public static function multiMap($groups)
+    {
+        $groupList = [];
+        foreach ($groups as $group) {
+            $groupList[] = self::map($group);
+        }
+
+        return $groupList;
+    }
 }
