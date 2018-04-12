0868ca2ff0a2f46f18fba564fc0c3cd7d9d2a2e8
Serhat Tan <94serhattan@gmail.com>
2018-04-12 21:18:25 +0300

    Fix user manager not found problem in add restaurant (WHAT-12)

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\RestaurantManager;
+use App\Models\RestaurantUserManager;
 
 class RestaurantController extends Controller
 {
